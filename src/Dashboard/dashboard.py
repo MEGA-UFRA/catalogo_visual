9a80e5cba18cd557f4ad20c28a121beeeadbca84
Marcia Fernanda <marciasouza199858@gmail.com>
2022-04-03 13:29:00 -0300

    Correção do dropdown e início da mudança do gráfico

--- src/Dashboard/dashboard.py
+++ src/Dashboard/dashboard.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output
 #from dash_core_components import Dropdown
 import plotly.express as px
 import pandas as pd
@@ -9,13 +9,15 @@ import pandas as pd
 app = Dash(__name__)
 
 #lendo a base de dados
-df = pd.read_csv(r'.\databases\PLANILHA_NELORE_ABCZ_GRUPO_2_80.csv', sep = ';')
+df = pd.read_csv('PLANILHA_NELORE_ABCZ_GRUPO_2_80.csv', sep = ';')
+drop_conteudo = list(df.columns)
+#drop_conteudo.append('Selecione')
+#print(list(df.columns))
 
 #criando os gráficos
 barras = px.bar(df, x = 'NOME', y = 'DEP_PA_ED', color = 'IABCZ', barmode = 'group')
 coordenadas = px.parallel_coordinates(df, color="IABCZ", dimensions=['DEP_PA_ED', 'DEP_PE_365', 'DEP_STAY', 'DEP_PA_ED', 'DEP_TMD', 'DEP_PE_450', 'DEP_ACAB', 'DEP_P'], color_continuous_scale= px.colors.diverging.delta)
 #opcoes = list(df['NOME'])
-#opcoes.append('Todos os dados')
 
 app.layout = html.Div(children=[
     html.H1(children = 'Dashboard Teste'),
@@ -23,17 +25,16 @@ app.layout = html.Div(children=[
     html.Div(children= '''
                         Coordenadas Paralelas e Profile Glifo
     '''),  
-    html.P(
+    html.Div(
             [
-            html.Label("Choose a feature"),
+            html.Label("Escolha as características: "),
             dcc.Dropdown(
                     id='demo-dropdown',                              
-                    options=['NYC', 'MTL', 'SF'],
-                    value='NYC',
+                    options = [{'label': x, 'value': x} for x in drop_conteudo],
+                    value = None,
                     multi=True
-                    )
-                    ]
-                    ),  
+                    )]),
+    html.Div(id = 'Selecao'),  
     dcc.Graph(
         id = 'B',
         figure = coordenadas
@@ -44,5 +45,23 @@ app.layout = html.Div(children=[
     )
 ])
 
+@app.callback(
+    Output('Selecao', 'children'),
+    Input('demo-dropdown', 'value')
+)
+
+def update_output(value):
+    print(value)
+    #return f'Você seleciounou: {value}'
+    if value == None:
+        fig = px.bar(df, x = 'NOME', y = 'DEP_PA_ED', color = 'IABCZ', barmode = 'group')
+    else:
+        df_filtrado = df.loc[:, df.columns==value] # loc serve para filtrar / : serve para indicar que se quer tudo
+        print(df_filtrado)
+        fig = px.bar(df, x = 'NOME', y = df_filtrado[0], color = 'IABCZ', barmode = 'group')
+
+    return fig
+
+
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True, use_reloader=False)
