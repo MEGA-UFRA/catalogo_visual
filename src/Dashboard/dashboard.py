8dd695b5a414b5a0e4e1282088aa4993a45c3997
Anderson Soares <anderson.gmarques@gmail.com>
2022-04-10 00:30:43 -0300

    Alterando coordenadas paralelas
    O coordenadas paralelas já está sendo plotado ao selecionar as características no dropdown menu.

--- src/Dashboard/dashboard.py
+++ src/Dashboard/dashboard.py
@@ -8,64 +8,86 @@ import pandas as pd
 
 app = Dash(__name__)
 
-#lendo a base de dados
+#============================================
+# LEITURA DA BASE DE DADOS
+#============================================
 df = pd.read_csv(r'databases\PLANILHA_NELORE_ABCZ_GRUPO_2_80.csv', sep = ';')
-drop_conteudo = list(df.columns)
-#drop_conteudo.append('Selecione')
-#print(list(df.columns))
+conteudo_dropdown = list(df.columns)
 
-#criando os gráficos
-barras = px.bar(df, x = 'NOME', y = 'DEP_PA_ED', color = 'IABCZ', barmode = 'group')
-coordenadas = px.parallel_coordinates(df, color="IABCZ", dimensions=['DEP_PA_ED', 'DEP_PE_365', 'DEP_STAY', 'DEP_PA_ED', 'DEP_TMD', 'DEP_PE_450', 'DEP_ACAB', 'DEP_P'], color_continuous_scale= px.colors.diverging.delta)
+
+#============================================
+# GRAFICOS
+#============================================
+coordenadas = px.parallel_coordinates(df, color="IABCZ", 
+                                      dimensions=['DEP_PA_ED', 'DEP_PE_365', 'DEP_STAY', 'DEP_PA_ED', 'DEP_TMD', 'DEP_PE_450', 'DEP_ACAB', 'DEP_P'], 
+                                      color_continuous_scale= px.colors.diverging.delta)
+barras = px.bar(df, x = 'NOME', y = 'DEP_PN_ED', color = 'NOME')
 #opcoes = list(df['NOME'])
 
+#============================================
+# LAYOUT
+#============================================
 app.layout = html.Div(children = [
-    html.H1(children = 'Dashboard Teste'),
-    html.H2(children = 'Dashboard com dados referentes a raça Nelore'),
-    html.Div(children= '''
-                        Coordenadas Paralelas e Profile Glifo
-    '''),  
+    html.H1(children = 'Dashboard - Catalogo visual'),
+    html.H2(children = 'Raça: Nelore'),
     html.Div(children = [
             html.Label("Escolha as características: "),
             dcc.Dropdown(
-                    id='demo-dropdown',                              
-                    options = [{'label': x, 'value': x} for x in drop_conteudo],
+                    id='dropdown_caracteristicas',
+                    options = [{'label': x, 'value': x} for x in conteudo_dropdown],
                     value = None,
                     multi=True
-                    )]
-                    ),
-    html.Div(id = 'Selecao')
-    # ,  
-    # dcc.Graph(
-    #     id = 'B',
-    #     figure = coordenadas
-    # )
-    # ,
-    # dcc.Graph(
-    #     id = 'A',
-    #     figure = barras
-    # )
+                       )]
+            ),
+    dcc.Graph(
+        id = 'coordenadas_paralelas',
+        figure = coordenadas
+             )
+    ,
+    dcc.Graph(
+        id = 'profile_glyph_A',
+        figure = barras
+    )
 ])
 
+#============================================
+# CALLBACKS
+#============================================
 @app.callback(
-    Output('Selecao', 'children'),
-    Input('demo-dropdown', 'value')
+    Output('coordenadas_paralelas', 'figure'),
+    Input('dropdown_caracteristicas', 'value')
 )
-
-def update_output(value):
-    print(f'Você seleciounou: {value}')
-    #return f'Você seleciounou: {value}'
+def update_coordenadas_paralelas(value):
     if value == None:
-        fig = px.bar(df, x = 'NOME', y = 'DEP_PA_ED', color = 'IABCZ', barmode = 'group')
+        fig = px.parallel_coordinates(df, color="IABCZ", 
+                                      dimensions=['DEP_PA_ED', 'DEP_PE_365', 'DEP_STAY', 'DEP_PA_ED', 'DEP_TMD', 'DEP_PE_450', 'DEP_ACAB', 'DEP_P'], 
+                                      color_continuous_scale= px.colors.diverging.delta)
     else:
-        df_filtrado = df.loc[:, df.columns==value] # loc serve para filtrar / : serve para indicar que se quer tudo
-        print(f'O que sai? {df.columns==value}')
-        print(f'Filtrado: {df_filtrado}')
-
-        # fig = px.bar(df, x = 'NOME', y = df_filtrado[0], color = 'IABCZ', barmode = 'group')
-        fig = px.bar(df, x = 'NOME', y = value, color = 'IABCZ', barmode = 'group')
+        fig = px.parallel_coordinates(df, color="IABCZ", 
+                                  dimensions=value, 
+                                  color_continuous_scale= px.colors.diverging.delta)
 
     return fig
+
+
+# @app.callback(
+#     Output('Selecao', 'children'),
+#     Input('demo-dropdown', 'value')
+# )
+# def update_grafico_barra(value):
+#     print(f'Você seleciounou: {value}')
+#     #return f'Você seleciounou: {value}'
+#     if value == None:
+#         fig = px.bar(df, x = 'NOME', y = 'DEP_PA_ED', color = 'IABCZ', barmode = 'group')
+#     else:
+#         df_filtrado = df.loc[:, df.columns==value] # loc serve para filtrar / : serve para indicar que se quer tudo
+#         print(f'O que sai? {df.columns==value}')
+#         print(f'Filtrado: {df_filtrado}')
+
+#         # fig = px.bar(df, x = 'NOME', y = df_filtrado[0], color = 'IABCZ', barmode = 'group')
+#         fig = px.bar(df, x = 'NOME', y = value, color = 'IABCZ', barmode = 'group')
+
+#     return fig
 
 
 if __name__ == '__main__':
